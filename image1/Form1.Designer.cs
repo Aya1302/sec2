188f82be63f9760eaf5fc1c2479964fa5ed01e73
Aya1302 <ayaelmasry64@gmail.com>
2024-12-06 14:09:53 +0200

    Add files via upload

---
+++ image1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace image1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.previous = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // previous
+            // 
+            this.previous.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.previous.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.previous.Location = new System.Drawing.Point(144, 350);
+            this.previous.Name = "previous";
+            this.previous.Size = new System.Drawing.Size(124, 46);
+            this.previous.TabIndex = 1;
+            this.previous.Text = "previous";
+            this.previous.UseVisualStyleBackColor = false;
+            this.previous.Click += new System.EventHandler(this.previous_Click);
+            // 
+            // next
+            // 
+            this.next.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.next.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.next.Location = new System.Drawing.Point(324, 350);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(133, 46);
+            this.next.TabIndex = 2;
+            this.next.Text = "next";
+            this.next.UseVisualStyleBackColor = false;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "10.jpg");
+            this.imageList1.Images.SetKeyName(1, "102.jpg");
+            this.imageList1.Images.SetKeyName(2, "106.jpg");
+            this.imageList1.Images.SetKeyName(3, "Ado.jpg");
+            this.imageList1.Images.SetKeyName(4, "stars.jpg");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(109, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 450);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.previous);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button previous;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ image1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image1
+{
+    public partial class Form1 : Form
+    {
+        int count = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(o.FileName);
+            }
+        }
+
+        private void previous_Click(object sender, EventArgs e)
+        {
+            if (count > 0)
+            {
+                count--;
+            }
+            pictureBox1.Image = imageList1.Images[count];
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {   
+            if (count < 4)
+            { count++; 
+            } 
+            pictureBox1.Image= imageList1.Images[count];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
